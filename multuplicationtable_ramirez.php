e04dc601bcdd57d9401a9c0b3a2a9aef3279b840
akoxitj123 <124246034+akoxitj123@users.noreply.github.com>
2023-03-28 12:05:13 +0800

    added multiplication table

---
+++ multuplicationtable_ramirez.php
@@ -0,0 +1,21 @@
+<html>
+<head>
+</head>
+<body>
+<?php
+// set the number for the multiplication table
+$num = 7;
+
+// generate the multiplication table using a for loop
+echo "Multiplication Table for " . $num . ":\n\n";
+echo "<ul style='list-style: none'>";
+for ($i = 1; $i <= 10; $i++) {
+  $result = $num * $i;
+  echo "<li>$num x  $i = " . $result . "</li>";
+  
+}
+echo "</ul>";
+?>
+
+</body>
+</html>
